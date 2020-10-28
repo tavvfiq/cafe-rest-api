bd0b9c1b0a17e48e77f406250f28344879ca3a86
tavvfiq <taufiq.w@mail.ugm.ac.id>
2020-10-28 23:15:29 +0700

    [update] add helpers for success response

--- apiHelpers/response.go
+++ apiHelpers/response.go
@@ -1,6 +1,16 @@
 package apihelpers
 
+import tb "github.com/tavvfiq/cafe-rest-api/database/mysql"
+
+// ErrorResponse helpers for returning error response from controllers
 type ErrorResponse struct {
 	Status  int    `json:"status"`
 	Message string `json:"msg"`
+}
+
+// SuccessResponse helpers for returning success response from controllers
+type SuccessResponse struct {
+	Status  int     `json:"status"`
+	Message string  `json:"msg"`
+	Data    tb.User `json:"data"`
 }
